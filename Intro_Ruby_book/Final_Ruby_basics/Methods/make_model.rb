aeb00f37caa2b7469d2280cf2d0118e3a813a939
mikepeters-ohio <ckn1901@icloud.com>
2021-08-31 22:17:15 -0400

    Tues commit

--- Intro_Ruby_book/Final_Ruby_basics/Methods/make_model.rb
+++ Intro_Ruby_book/Final_Ruby_basics/Methods/make_model.rb
@@ -1,1 +1,17 @@
-#Make and model exercise 
+#Make and model exercise
+
+=begin, this is incorrect
+car('Toyota', 'Corolla')
+
+def car 
+  'Toyota Corolla' 
+end  
+
+puts car
+=end 
+
+def car(make, model)
+  puts "#{make} #{model}"
+end
+
+car('Toyota', 'Corolla') #this is correct
