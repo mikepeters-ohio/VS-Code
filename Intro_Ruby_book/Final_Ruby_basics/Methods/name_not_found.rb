0a6e8153f6053504f1d52113fe5742bb8173a1e2
mikepeters-ohio <ckn1901@icloud.com>
2021-09-03 05:31:14 -0400

    Fri Sep 3 commit

---
+++ Intro_Ruby_book/Final_Ruby_basics/Methods/name_not_found.rb
@@ -0,0 +1,18 @@
+#Name not found 
+
+=begin 
+def not_required Bob
+  if true then
+     puts "true" * 2
+  end 
+
+puts assignment_name('Kevin') == 'Kevin' #nomethoderror
+puts assignment_name == 'Bob'
+=end
+
+def assign_name (name = 'Bob')
+  name
+end 
+
+puts assign_name('Kevin') == Kevin
+puts assign_name == 'Bob'
---
+++ Intro_Ruby_book/Final_Ruby_basics/Methods/naming_animals.rb
@@ -0,0 +1,12 @@
+#Naming animals method exercise
+
+def dog (name)
+  return name
+end 
+
+def cat (name)
+  return name
+end 
+
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat('Puss in boots')}."
