c957d92e9c3496bd1464978e519e73647ee30385
mikepeters-ohio <ckn1901@icloud.com>
2021-09-05 22:11:55 -0400

    Sep5 commiti

---
+++ Intro_Ruby_book/Final_Ruby_basics/Return/bld5.rb
@@ -0,0 +1,8 @@
+#Breakfast, Lunch, and Dinner part 5.
+
+def meal
+  'Dinner'
+  puts 'Dinner'
+end
+
+p meal
---
+++ Intro_Ruby_book/Final_Ruby_basics/Return/bld6.rb
@@ -0,0 +1,11 @@
+#Breakfast, Lunch, Dinner part 6  
+
+def meal
+  return 'Breakfast'
+  'Dinner'
+  puts 'Dinner'
+end 
+
+puts meal #if you move this to line 3, it returns a (NameError)
+
+#This puts whatever is immediately after return.
