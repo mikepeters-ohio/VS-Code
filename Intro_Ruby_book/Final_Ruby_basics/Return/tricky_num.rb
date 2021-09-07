35e3b558aa623960c7a8d2f378943f63be6bb16b
mikepeters-ohio <ckn1901@icloud.com>
2021-09-06 21:20:56 -0400

    Sep 6 21:20 commit

---
+++ Intro_Ruby_book/Final_Ruby_basics/Return/tricky_num.rb
@@ -0,0 +1,11 @@
+#Tricky Number exercise
+
+def tricky_number
+  if true 
+    number = 1
+  else #If lines 6-7 are eliminated the answer does not change.
+    2 #Thus, these lines appear to be worthless
+  end
+end 
+
+puts tricky_number
