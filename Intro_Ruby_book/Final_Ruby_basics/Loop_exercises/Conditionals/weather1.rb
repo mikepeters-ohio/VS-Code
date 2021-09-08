dc9b43055fbb74b461c6d6d45d7684ef8e1fc56a
mikepeters-ohio <ckn1901@icloud.com>
2021-09-07 21:38:35 -0400

    Sep 7 21:38 commit

--- Intro_Ruby_book/Final_Ruby_basics/Loop_exercises/Conditionals/weather1.rb
+++ Intro_Ruby_book/Final_Ruby_basics/Loop_exercises/Conditionals/weather1.rb
@@ -1,2 +1,7 @@
 #Unpredictable weather (part 1)
 
+sun = ['visible', 'hidden'].sample
+
+if sun == 'visible'
+  p "The sun is so bright!"
+end 
---
+++ Intro_Ruby_book/Final_Ruby_basics/Loop_exercises/Conditionals/weather2.rb
@@ -0,0 +1,8 @@
+#Unpredictable Weather (Part 2)
+
+sun = ['visible', 'hidden'].sample
+
+unless
+end 
+
+puts
