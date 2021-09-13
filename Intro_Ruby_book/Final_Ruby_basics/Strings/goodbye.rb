d9ad7a54f30f2bdd8dd7be10d0c6799fc5eedafa
mikepeters-ohio <ckn1901@icloud.com>
2021-09-13 05:04:30 -0400

    Sep13 05:05 commit

---
+++ Intro_Ruby_book/Final_Ruby_basics/Strings/goodbye.rb
@@ -0,0 +1,9 @@
+#Goodbye, not hello exercise
+
+greeting = 'Hello!'
+
+greeting.gsub!('Hello', 'Goodbye')
+puts greeting 
+
+##gsub! allows you to replace characters 
+#in strings by modifying the original object  
---
+++ Intro_Ruby_book/Final_Ruby_basics/Strings/print_alpha.rb
@@ -0,0 +1,8 @@
+# Print the Alphabet
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+puts = alphabet.split('d')
+
+#Why does #split know to split 25 times? Why not once?
+
+##chars does something similar 
