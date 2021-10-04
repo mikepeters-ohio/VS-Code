846c8b346e7402b1b389e452274f5ac3e66a0714
mikepeters-ohio <ckn1901@icloud.com>
2021-10-04 17:35:05 -0400

    Oct4 commit

--- Intro_Ruby_book/Final_Ruby_basics/Loop2.exercises/even_odd.rb
+++ Intro_Ruby_book/Final_Ruby_basics/Loop2.exercises/even_odd.rb
@@ -3,12 +3,16 @@
 count = 1
 
 loop do 
-  if count.even?
+  if count.even? #'if count % 2 == 0' is also right
     puts "#{count} is even!"
   else
     puts "#{count} is odd!"
-  end
+  end #Ternary operator can also be used
 
   break if count == 5
   count += 1
 end
+
+=begin
+Line 13 tells us that the count adds 1 with every iteration. 
+Line 3 tells us that the count starts at 1. 
