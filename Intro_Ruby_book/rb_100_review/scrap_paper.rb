0907057b7c1a505bc32209346ea2ee525028f725
mikepeters-ohio <ckn1901@icloud.com>
2021-10-08 21:50:22 -0400

    Oct8 commit

---
+++ Intro_Ruby_book/rb_100_review/scrap_paper.rb
@@ -0,0 +1,28 @@
+#name.rb
+
+=begin
+#puts "What is your name?"
+  #answer = gets.chomp
+  #puts "Good morning #{answer}, I am Gul Dukat."
+
+#age.rb
+
+puts "how old are you?"
+  age = gets.chomp.to_i
+  puts "In 10 years you will be #{age + 10}"
+  
+  puts "In 20 years you will be #{age + 20}"
+  puts "In 30 years you will be #{age + 30}"
+  puts "In 40 years you will be #{age + 40}" 
+=end 
+
+def dog(name)
+  return name
+end
+
+def cat(name)
+  return name
+end
+
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat('Ginger')}."
