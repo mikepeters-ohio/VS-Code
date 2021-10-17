b7fbaacf0faaa58cec3033d1fa9761f5be95bd86
mikepeters-ohio <ckn1901@icloud.com>
2021-10-16 21:37:18 -0400

    Oct16_21:37_commit

---
+++ rb101_small_problems/Documentation/exercise1.rb
@@ -0,0 +1,8 @@
+=begin 
+Notes: The 1st (File::path) returns the string representation of the path. 
+  The 2nd (File#path) gets called  on the object of the file class.
+  I need these answers translated for me, or was I supposed to start 
+  lesson 2 first? 
+PEDAC: n/a 
+=end 
+
