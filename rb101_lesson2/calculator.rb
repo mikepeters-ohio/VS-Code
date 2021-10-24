dba7a5d012317c937368e532cd9db1d368e87d96
mikepeters-ohio <ckn1901@icloud.com>
2021-10-23 21:45:06 -0400

    Oct23 21:44 commit

--- rb101_lesson2/calculator.rb
+++ rb101_lesson2/calculator.rb
@@ -20,6 +20,14 @@ Kernel.puts ("What operation would you like to perform? 1) add 2) subtract 3) mu
 operator = Kernel.gets().chomp()
 
 if operator == '1'
-  result = number1.to_i() + number2.to_1()
-elsif
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+else operator == '4'
+  result = number1.to_f() / number2.to_f()
+
 end
+
+Kernel.puts("The result is #{result}")
