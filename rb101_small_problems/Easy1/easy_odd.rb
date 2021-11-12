c6d3286c1eb784d4906e7e40a05ce085553e631b
mikepeters-ohio <ckn1901@icloud.com>
2021-11-11 22:06:50 -0500

    Nov11 2206 commit

---
+++ rb101_small_problems/Easy1/easy_odd.rb
@@ -0,0 +1,17 @@
+#Odd exercise 
+=begin
+def number
+  if odd 
+    puts true 
+  else
+    puts false 
+  end
+end 
+
+number = 2
+  puts number
+=end 
+
+def is_odd?(number)
+  number % 2 == 1
+end
