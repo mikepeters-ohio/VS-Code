19a17650ef2290396c4b676cee8c7795f1bd778d
Mike P <87240671+mikepeters-ohio@users.noreply.github.com>
2021-11-20 18:52:44 -0500

    Nov20 1852 commit

---
+++ rb101_lesson2/re_calculator.rb
@@ -0,0 +1,29 @@
+#Refactoring Calculator
+
+def prompt(message) 
+  Kernel.puts ("=> #{message}")
+end
+
+prompt("Welcome to calculator!") 
+
+prompt("What's the first number?")
+number1 = Kernel.gets().chomp()
+
+prompt("What's the second number?")
+number2 = Kernel.gets().chomp()
+
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+else operator == '4'
+  result = number1.to_f() / number2.to_f()
+
+end
+
+prompt("The result is #{result}")
