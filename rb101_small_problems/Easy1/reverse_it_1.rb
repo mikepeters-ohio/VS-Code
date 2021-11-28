5ecbaf017c778cc7eec75fd5a386f6c8d5cc10aa
Mike Peters <ckn1901@icloud.com>
2021-11-27 22:23:29 -0500

    Nov27 2223 commit

--- rb101_small_problems/Easy1/reverse_it_1.rb
+++ rb101_small_problems/Easy1/reverse_it_1.rb
@@ -3,12 +3,20 @@
 =begin
 Write a method that takes one argument, a string, 
 and returns a new string with the words in reverse order.
+=end 
+
+def reverse_sentence(string)
+  string.split.reverse.join(' ')
+end
 
 puts reverse_sentence('Hello World') == 'World Hello'
 puts reverse_sentence('Reverse these words') == 'words these Reverse'
 puts reverse_sentence('') == ''
 puts reverse_sentence('    ') == '' # Any number of spaces results in ''
-=end 
+ 
 
+=begin 
 def reverse_sentence (words)
-  words.reverse
+  words.gets().chomp().reverse
+  puts words
+=end
