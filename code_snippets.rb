6deb6707c8e8a0393a9e10f5c962932c19e466bc
Mike Peters <ckn1901@icloud.com>
2021-12-12 22:00:17 -0500

    Dec 12 2200 commit

--- code_snippets.rb
+++ code_snippets.rb
@@ -4,6 +4,11 @@
   puts 'Hip Hip Horray!'
 #end
 
+=begin
+  
+rescue => exception
+  
+end
 USERNAME = 'Batman'
 
 def authenticate
@@ -12,3 +17,13 @@ end
 
 authenticate    # => Logging in Batman
 
+
+names = ['kim', 'joe', 'sam']
+names.each { |_| puts "got a name!" }
+=end 
+
+names.each_with_index do|_, idx|
+  puts "#{idx+1}. got a name!"
+end #Acc. to LS, this should work. 
+#The 2 names method above ARE ex. of 
+#using underscore for unused parameters.
