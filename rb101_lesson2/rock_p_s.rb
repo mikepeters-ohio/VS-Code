a5d2be6b87418dace2e7b61c9143f349b3470dc3
Mike Peters <ckn1901@icloud.com>
2021-12-12 21:47:58 -0500

    Dec 12 commit

---
+++ rb101_lesson2/rock_p_s.rb
@@ -0,0 +1,20 @@
+#Rock, Paper, Scissors
+
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  Kernel.puts("=> {message}")
+end 
+
+loop do
+  prompt("Choose one: #{VALID_CHOICES.join(', )}")
+  choice = Kernel.gets().chomp()
+
+  if VALID_CHOICES.include?(choice)
+    break 
+  else 
+    prompt("That's not a valid choice.")
+  end
+end 
+
+computer_choice = VALID_CHOICES.sample()
