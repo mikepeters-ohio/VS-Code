9342eee72e202b2b702309804db7590a10608c87
Mike Peters <ckn1901@icloud.com>
2021-12-18 12:48:55 -0500

    Dec18 1248 commit

--- rb101_lesson2/mortgage_calculator.rb
+++ rb101_lesson2/mortgage_calculator.rb
@@ -4,17 +4,17 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-loop do 
+loop do
   prompt("Welcome to the Bailey Bros. mortgage calculator!")
   prompt("------------------------------------------------")
 
   prompt("What is the loan amount?")
 
   amount = ''
-  loop do 
+  loop do
     amount = Kernel.gets().chomp()
 
-    if amount.empty?() || amount.to_f() < 0 
+    if amount.empty?() || amount.to_f() < 0
       prompt("Please enter a positive number.")
     else
       break
@@ -24,44 +24,45 @@ loop do
   prompt("What is the interest rate?")
   prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
 
-  interest rate = ''
+  interest = ''
   loop do
     interest_rate = Kernel.gets().chomp()
 
     if interest_rate.empty?() || amount.to_f() < 0
-     prompt("Please enter a positive number.")
-   else
+      prompt("Please enter a positive number.")
+    else
      break
     end
   end
 
-  prompt("How long do you want the loan to be (in years)?") 
-  
+  prompt("How long do you want the loan to be (in years)?")
+
   years = ''
-  loop do 
+  loop do
     years = Kernel.gets().chomp()
 
     if years.empty?() || years.to_i() < 0
-     prompt("Please enter a positive number.")
+      prompt("Please enter a positive number.")
     else
       break
     end
   end
 
   annual_interest_rate = interest_rate.to_f() / 100
-  monthly_interest_rate = annual_interest_rate /12
+  monthly_interest_rate = annual_interest_rate / 12
   months = years.to_i() * 12
 
-  monthly_payment = amount.to_f()*
+  monthly_payment = amount.to_f() *
                   (monthly_interest_rate / 
                   (1 - (1 + monthly_interest_rate)**(-months)))
 
-  prompt("Your monthly payment is: $#{format(%.2f , monthly_payment)}")
+  prompt("Your monthly payment is: $#{format(%. 2f , monthly_payment)}")
 
   prompt("Another calculation?")
   answer = Kernel.gets().chomp() 
 
   break unless answer.downcase().start_with?('y')
+  
 end
 
 prompt("Thank you for using the Bailey Bros. Mortgage Calculator!")
