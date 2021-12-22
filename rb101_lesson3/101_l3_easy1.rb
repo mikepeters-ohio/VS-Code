4f25fb87c985c5c73a7c7f1c82e63f8711fe4e0b
Mike Peters <ckn1901@icloud.com>
2021-12-22 14:50:10 -0500

    Dec 22 1448 commit

--- rb101_lesson3/101_l3_easy1.rb
+++ rb101_lesson3/101_l3_easy1.rb
@@ -1,6 +1,43 @@
 =begin 
+Question 1
 numbers = [1, 2, 2, 3]
 numbers.uniq
 
 puts numbers
+
+#Question 2
+
+The difference between ! and ? is ...
+
+1. This means "does not equal" and ... IS MOSTLY USED W/ INTEGERS
+2.
+3.
+4. 
+5.
+6. 
+
 =end
+
+#Question 3 - insert "urgent"
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+#Question 4
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers.delete_at(1)
+numbers.delete(1)
+
+#Question 5 - Programmatically determine if 42 lies between 10 and 100.
+#Question 6 - Add "four score and " to the front
+
+famous_words = "seven years ago..."
+
+#Question 7 - un-nest this array!
+
+["Fred", "Wilma", ["Barney", "Betty"], ["BamBam", "Pebbles"]]
+
+#Question 8 - Convert this hash to an array just about Barney.
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
