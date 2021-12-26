9b8231ef9dc551331e94bd5426b09857da9445f4
Mike Peters <ckn1901@icloud.com>
2021-12-25 21:21:26 -0600

    Dec25 2121 commit

--- rb101_lesson3/101_l3_easy1.rb
+++ rb101_lesson3/101_l3_easy1.rb
@@ -7,37 +7,54 @@ puts numbers
 
 #Question 2
 
-The difference between ! and ? is ...
+The difference between ! and ? is VAST! ! is the bang operator, but ? itself 
+is not an operator (it is part of the ternary operator syntax , however). 
+! is a method, but ? is not.  ? is also used to used to change 
+the function of some Ruby methods. 
+(https://www.tutorialspoint.com/ruby/ruby_operators.htm)
 
-1. This means "does not equal" and ... IS MOSTLY USED W/ INTEGERS
+1. The logical not operator. This means "does not equal" and will turn something 
+true to false
 2.
-3.
+3. ! at the end of a method means the method can do serious change or
+   damage to your code.
 4. 
-5.
-6. 
+5. Methods with a '?' return a Boolean - which makes the code flow even
+ more intuitively like a sentence.  
+6. Double Negation converts a value to Boolean, but this is not usually advisable.
 
 =end
 
 #Question 3 - insert "urgent"
 
-advice = "Few things in life are as important as house training your pet dinosaur."
+#advice = "Few things in life are as important as house training your pet dinosaur."
+
+#advice.gsub! 'important', 'urgent'
+#puts advice 
 
 #Question 4
 
-numbers = [1, 2, 3, 4, 5]
+#numbers = [1, 2, 3, 4, 5]
 
-numbers.delete_at(1)
-numbers.delete(1)
+#numbers.delete_at(1)
+#numbers.delete(1)
 
 #Question 5 - Programmatically determine if 42 lies between 10 and 100.
+
+def life
+  ("10".."100").include?("42")
+end
+
+puts life 
+
 #Question 6 - Add "four score and " to the front
 
-famous_words = "seven years ago..."
+#famous_words = "seven years ago..."
 
 #Question 7 - un-nest this array!
 
-["Fred", "Wilma", ["Barney", "Betty"], ["BamBam", "Pebbles"]]
+#["Fred", "Wilma", ["Barney", "Betty"], ["BamBam", "Pebbles"]]
 
 #Question 8 - Convert this hash to an array just about Barney.
 
-flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+#flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
