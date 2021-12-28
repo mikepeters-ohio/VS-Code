c74e07064ee73a8c3824041c4a2729b0ad81f6c8
Mike Peters <ckn1901@icloud.com>
2021-12-28 08:15:06 -0500

    Dec28 0814 commit

--- rb101_lesson3/101_l3_easy2.rb
+++ rb101_lesson3/101_l3_easy2.rb
@@ -18,13 +18,15 @@
 
 #2. munsters_description = "The Munsters are creepy in a good way."
 
-Convert the string in the following ways (code will be executed 
+
+
+#Convert the string in the following ways (code will be executed 
   on original munsters_description above):
 
 "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
 "The munsters are creepy in a good way."
-"the munsters are creepy in a good way."
-"THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+#"the munsters are creepy in a good way." munsters_description.downcase!
+#"THE MUNSTERS ARE CREEPY IN A GOOD WAY." munsters_description.upcase! 
 
 =begin 
 #3. Add these 2 hashes together:
