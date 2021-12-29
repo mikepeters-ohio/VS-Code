c6847327787f2c6f5f25e5fbd98854e7ce689e02
Mike Peters <ckn1901@icloud.com>
2021-12-28 21:57:54 -0500

    Dec28 2157 commit

--- rb101_lesson3/101_l3_easy2.rb
+++ rb101_lesson3/101_l3_easy2.rb
@@ -15,16 +15,17 @@
 
 #ages.fetch("Spot" , "Spot isn't here!")
 
+#This works, too: ages.fetch('Spot', false)
 
 #2. munsters_description = "The Munsters are creepy in a good way."
 
 
 
 #Convert the string in the following ways (code will be executed 
-  on original munsters_description above):
+#on original munsters_description above):
 
-"tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
-"The munsters are creepy in a good way."
+#"tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+#"The munsters are creepy in a good way."
 #"the munsters are creepy in a good way." munsters_description.downcase!
 #"THE MUNSTERS ARE CREEPY IN A GOOD WAY." munsters_description.upcase! 
 
@@ -33,18 +34,54 @@
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
 additional_ages = { "Marilyn" => 22, "Spot" => 237 }
 
+def ages do 
+  ages.merge * additional_ages
+  puts ages 
+end 
+
 #4. See if the name "Dino" appears in the string below:
+
 advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice.include? "Dino"
+  puts advice
+end 
 
 #5. Show an easier way to write this array:
 flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
 
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+=end 
+
 #6. Add the family pet Dino:
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 
-#7. *go to website*
+#flintstones.push("Dino") 
+  #puts flintstones
 
+  #flintstones << "Dino" - BEST 
+
+=begin
+  
+rescue => exception
+  
+end
+
+#7. flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+    flintstones << "Dino"
+
+    flintstones.push("Dino").push("Hoppy")   # push returns the array so we can chain
+    OR flintstones.concat(%w(Dino Hoppy))  # concat adds an array rather than one item
+=end 
+
+=begin
+  
+rescue => exception
+  
+end
 #8. Shorten the sentence by doing this: 
+
 advice = "Few things in life are as important as house training your pet dinosaur."
 
 #Review the String#slice! documentation, and use that method to make the return value 
@@ -53,10 +90,14 @@ advice = "Few things in life are as important as house training your pet dinosau
 
 #As a bonus, what happens if you use the String#slice method instead?
 
+
 #9. Write a one-liner to count the number of lower-case 't' characters 
 in the following string:
 
 statement = "The Flintstones Rock!"
+statement.count('t')
+=end 
+
 
 #10. Back in the stone age (before CSS) we used spaces to align things 
 #on the screen. If we had a table of Flintstone family members that was 
@@ -65,4 +106,5 @@ statement = "The Flintstones Rock!"
 
 title = "Flintstone Family Members"
 
-=end 
+"Flintstone Family Members".center(40)
+   
