5ab2ac01f6022ec49adcaebe774854eb1a896b08
Mike Peters <ckn1901@icloud.com>
2022-01-02 18:32:54 -0500

    Jan2 1832 commit

--- rb101_lesson3/l3_med1_fib.rb
+++ rb101_lesson3/l3_med1_fib.rb
@@ -4,6 +4,8 @@ limit = 15
 
 def fib(first_num, second_num)
   while first_num + second_num < limit
+    unless limit > 15
+      break
     sum = first_num + second_num
     first_num = second_num
     second_num = sum
