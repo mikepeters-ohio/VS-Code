b99ae751098569b940e31c5f186850cf8dddaa8b
Mike Peters <ckn1901@icloud.com>
2022-01-02 16:41:19 -0500

    Jan2 1641 commit

--- rb101_lesson3/101_l3_med1.rb
+++ rb101_lesson3/101_l3_med1.rb
@@ -5,7 +5,7 @@
 
 10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
 
-=end
+
 
 #2 Why is this an error and 2 fixes? 
 
@@ -21,14 +21,60 @@
 def factors(number)
   divisor = number
   factors = []
-  begin
+  loop
     factors << number / divisor if number % divisor.zero?
     divisor -= 1
-  end until divisor == 0
+  break divisor.zero?
   factors
 end
 
 # Two bonus questions 
+
+# What does number % divisor == 0 mean? A:Allows you to determine if 
+#the result of the division is an integer number (no remainder).
+
+#What is the purpose of the 'factors' before the method's 'end'?
+#A:This is what is the actual return value from the method. Recall that 
+#without an explicit return statement in the code, the return value of 
+#the method is the last statement executed. If we omitted this line, 
+#the code would execute, but the return value of the method would be nil.
+
+#4 #+ returns a new array, but << does not. #<< appends the object to self 
+#and returns self.
+
+
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+#I think the first program is correct, because the second creates two arrays,
+# and the questions does not appear to ask for you arrays, but one. 
+
+=end 
+
+#5. Fib calculator 
+
+limit = 15
+
+def fib(first_num, second_num)
+  while first_num + second_num < limit
+    unless #??sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1)
+puts "result is #{result}"
 
 =begin
 #6 
