3a4dbca7d609ddb51a91875e23f07584b763864f
Mike Peters <ckn1901@icloud.com>
2022-01-03 21:55:43 -0500

    Jan3 2155 commit

--- rb101_small_problems/Easy1/list_of_digits.rb
+++ rb101_small_problems/Easy1/list_of_digits.rb
@@ -1,1 +1,23 @@
-# sort! method? 
+#sort! method? 
+
+number = '12345'.split('')
+  number.sort! 
+  puts number #wrong
+
+=begin
+Correct (brute force)
+def digit_list(number)
+  digits = []
+  loop do
+    number, remainder = number.divmod(10)
+    digits.unshift(remainder)
+    break if number == 0
+  end
+  digits
+end
+
+Idiomatic Ruby (also correct)
+def digit_list(number)
+  number.to_s.chars.map(&:to_i)
+end
+=end 
