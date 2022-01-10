a67e60626500c4174b31f046c032e7bb1f7f865d
Mike Peters <ckn1901@icloud.com>
2022-01-09 21:11:27 -0500

    Jan9 2111 commit

--- scrap_paper.rb
+++ scrap_paper.rb
@@ -7,10 +7,20 @@ loop do
 end 
 
 puts var
-=end 
+ 
 
 #puts "It's true!" if some_method_call 
 
 [1, 2, 3].each do |num|
   puts num
+=end
+
+require "pry" # add this to use Pry
+
+counter = 0
+
+loop do
+  counter += 1
+  binding.pry # execution will stop here
+  break if counter == 5
 end
