657488ce2ab1da8a67c746ad87a44bb2c9617fee
Mike Peters <ckn1901@icloud.com>
2022-01-16 04:53:02 -0500

    Jan16 0452 commit

---
+++ rb101_small_problems/Easy1/array_average.rb
@@ -0,0 +1,10 @@
+#input - one argument, an array of integers
+#output - the average of all integers in the array 
+
+#rules - The array will never be empty and the numbers always positive integers. 
+
+#edge cases - How to handle negative integers
+
+#Data structures - Array 
+
+#Algorithm - 
