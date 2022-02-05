d9558196dadd03ce28611226c9d3aa9a4c09f06d
Mike Peters <ckn1901@icloud.com>
2022-02-05 14:07:51 -0500

    Feb5 1407 commit

--- rb101_lesson4/rb101_l4_notes.rb
+++ rb101_lesson4/rb101_l4_notes.rb
@@ -34,6 +34,7 @@ end
 arr # => [2, 3, 4, 5, 6]
 =end
 
+=begin 
 loop do
   number = rand(1..10)   # a random number between 1 and 10
   puts 'Hello!'
@@ -42,10 +43,34 @@ loop do
     break
   end
 end
+=end 
 
 #Observations of running the above
 #random number of outputs
 #My 2nd attempt exited after just 3 iterations, so the code doesn't seem to work
 # Line 40, 'if number == 5' doesn't work at all 
 # 10 iterations does seem to be the limit, so line 38 works
-#Consider moving 'break' to line 41. 
+#Consider moving 'break' to line 41.
+
+#Looping 
+=begin 
+arr = [1, 2, 3, 4, 5]
+counter = 0
+
+loop do 
+  arr[counter] += 1
+  counter += 1
+  break if counter == arr.size #same as 'length' here, it will return 5 digits.
+end 
+
+arr 
+=end 
+
+counter = 0
+
+loop do 
+  puts "Gryffidor WINS!"
+  counter += 1
+  break if counter == 2
+end 
+
