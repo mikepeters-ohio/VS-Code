29f26f2b5423d1f099d01db775405adfc9c2fe12
Mike Peters <ckn1901@icloud.com>
2022-02-20 13:33:41 -0500

    Feb20 commit

--- rb101_small_problems/Easy1/sum_of_digits.rb
+++ rb101_small_problems/Easy1/sum_of_digits.rb
@@ -1,1 +1,10 @@
 #RB101 - 109 Sm. Problems, Easy 1, "Sum of Digits"
+
+#If you had done PEDAC, there's a small chance you would have gotten farther. 
+def method (sum)
+digits = sum {digits + digits}
+end 
+
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
