4d3b851d2b9f0a1ff56e7141e2c3c31cd5d0ccec
Mike Peters <ckn1901@icloud.com>
2022-03-03 22:43:57 -0500

    Mar3 2243 commit

---
+++ rb101_small_problems/Easy2/even_numbers.rb
@@ -0,0 +1,13 @@
+value = 2
+while value <= 99
+  puts value
+  value += 2
+end
+
+||
+
+value = 1
+while value <= 99
+  puts value if value.even?
+  value += 1
+end 
---
+++ rb101_small_problems/Easy2/odd_numbers.rb
@@ -0,0 +1,15 @@
+#RB 101 Easy 2 "Odd Numbers" 
+#PEDAC
+
+#input - ?
+#output - odd numbers 1-99 
+#edge cases - negative numbers?
+#Data Structure - integers
+#Algorithm - if a number 1-99 is odd print it to the console 
+#Code 
+
+value = 1
+while value <= 99
+  puts value
+  value += 2
+end
