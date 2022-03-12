6049706b81171d5574e85a15a469e2bddaf6a12c
Mike Peters <ckn1901@icloud.com>
2022-03-11 20:38:57 -0500

    Mar11 2038 commit

--- rb101_lesson5/lesson5_scrap_paper.rb
+++ rb101_lesson5/lesson5_scrap_paper.rb
@@ -1,3 +1,12 @@
+=begin 
 arr = [[1, 3], [2]]
 arr[1] = "hi there"
 p arr 
+=end 
+
+[{ a: 'ant', b: 'elephant' }, { c: 'cat' }].select do |hash|
+  hash.all? do |key, value|
+    value[0] == key.to_s
+  end
+end
+# => [{ :c => "cat" }]
