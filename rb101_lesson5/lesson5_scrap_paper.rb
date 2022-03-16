e8bfc395d64618b689825da382b89b2b2ac69928
Mike Peters <ckn1901@icloud.com>
2022-03-15 23:45:30 -0400

    Mar16 commit

--- rb101_lesson5/lesson5_scrap_paper.rb
+++ rb101_lesson5/lesson5_scrap_paper.rb
@@ -2,7 +2,7 @@
 arr = [[1, 3], [2]]
 arr[1] = "hi there"
 p arr 
-=end 
+ 
 
 [{ a: 'ant', b: 'elephant' }, { c: 'cat' }].select do |hash|
   hash.all? do |key, value|
@@ -10,3 +10,53 @@ p arr
   end
 end
 # => [{ :c => "cat" }]
+ 
+
+arr = [
+  {one: '1', two: 2},
+  [{four: 5, three: 6}, :three],
+  :three,
+  {2=>:two, 3=>:three}
+]
+arr[1][0][:three]
+
+
+todo_lists = [
+  {
+    id: 1,
+    list_name: 'Groceries',
+    todos: [
+      { id: 1, name: 'Bread', completed: false },
+      { id: 2, name: 'Milk', completed: false },
+      { id: 3, name: 'Apple Juice', completed: false }
+    ]
+  }
+]
+
+todo_lists[0][:todos][2] = 'Orange Juice'
+
+arr = [
+  {one: '1', two: 2},
+  [{four: 5, three: 6}, :three],
+  :three,
+  {2=>:two, 3=>:three}
+]
+
+puts arr[3][3]
+
+=end 
+
+todo_lists = [
+  {
+    id: 1,
+    list_name: 'Groceries',
+    todos: [
+      { id: 1, name: 'Bread', completed: false },
+      { id: 2, name: 'Milk', completed: false },
+      { id: 3, name: 'Apple Juice', completed: false }
+    ]
+  }
+]
+todo_lists[0][:todos][2][:name] = 'Orange Juice'
+
+
