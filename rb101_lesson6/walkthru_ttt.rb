f63a90eb8221dd56eccd11898f07ad4e2392b8a1
Mike Peters <ckn1901@icloud.com>
2022-03-21 05:15:25 -0400

    Mar21 0515 commit

--- rb101_lesson6/walkthru_ttt.rb
+++ rb101_lesson6/walkthru_ttt.rb
@@ -1,7 +1,34 @@
 #"Walk through: Tic Tac Toe"
 
 =begin
+More than one way to build this game
 
+def display_board(brd)
+  puts ""
+  puts "    |    | "
+  puts " #{brd[1]}   |    | "
+  puts "    |    | "
+  puts "--------------- "
+  puts "    |    | "
+  puts "    |    | "
+  puts "    |    | "
+  puts "--------------- "
+  puts "    |    | "
+  puts "    |    | "
+  puts "    |    | "
+  puts "    |    | "
+end 
 
+def intialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] ='X'}
+  new_board
+end
 
+board = intialize_board
+display_board(board)
+
+{1 => ' ', 2 => ' ', 3 => ' ', 4 => ' '}
+
+puts ""
 =end 
