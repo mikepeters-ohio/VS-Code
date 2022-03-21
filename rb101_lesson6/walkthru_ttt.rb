6a236c163f9dbf115ccc5ad646fa26a66b830f47
Mike Peters <ckn1901@icloud.com>
2022-03-21 05:47:51 -0400

    Mar21 0547 commit

--- rb101_lesson6/walkthru_ttt.rb
+++ rb101_lesson6/walkthru_ttt.rb
@@ -4,24 +4,24 @@
 More than one way to build this game
 
 def display_board(brd)
-  puts ""
+  puts " "
   puts "    |    | "
-  puts " #{brd[1]}   |    | "
-  puts "    |    | "
-  puts "--------------- "
-  puts "    |    | "
-  puts "    |    | "
+  puts " #{brd[1]}   | #{brd[2]   | #{brd[3] "
   puts "    |    | "
   puts "--------------- "
   puts "    |    | "
+  puts " #{brd[4]   | #{brd[5]   |  #{brd[6] "
   puts "    |    | "
+  puts "--------------- "
   puts "    |    | "
+  puts " #{brd[7]   | #{brd[8]   |  #{brd[9]"
   puts "    |    | "
+
 end 
 
 def intialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] ='X'}
+  (1..9).each {|num| new_board[num] =''}
   new_board
 end
 
