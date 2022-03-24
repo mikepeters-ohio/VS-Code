2a814ef6270fe8ae51096522e3a2ca09715c0981
Mike Peters <ckn1901@icloud.com>
2022-03-23 22:18:18 -0400

    Mar23 commit

--- rb101_lesson6/walkthru_ttt.rb
+++ rb101_lesson6/walkthru_ttt.rb
@@ -1,7 +1,12 @@
 #"Walk through: Tic Tac Toe"
 
+
+#More than one way to build this game
+
 =begin
-More than one way to build this game
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
 
 def prompt(msg) 
   puts "=> #{msg}"
@@ -25,21 +30,43 @@ end
 
 def intialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] =''}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
   new_board
+
+def empty_squares(brd)
+   brd.keys.select{|num| brd[num] == INITIAL_MARKER}
 end
 
-def player_places_piece(brd)
-  prompt "Choose a square (1-9): "
-  square = gets.chomp
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+  brd[square] = 'PLAYER_MARKER'
+end 
+
+def comp_places_piece!(board) 
+  square = empty_squares(brd).sample
+  brd[]
+end 
+
+def board_full?()
+# -5:16 mark in Step 3 film 
 end 
 
 board = intialize_board
 display_board(board)
 
-player_places_piece(board) #you want mutation! 
-
+loop do 
+  player_places_piece!(board) #you want mutation! 
+  comp_places_piece!(board)
+#puts board.inspect
+  display_board(board)
+  break if someone_won? || board_full?(board)
 {1 => ' ', 2 => ' ', 3 => ' ', 4 => ' '}
 
-puts ""
-=end 
+puts " "
+=end
