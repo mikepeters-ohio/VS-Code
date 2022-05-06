c59b3e1a3238675bc76fc61a63d8df7d91cd6b27
Mike Peters <ckn1901@icloud.com>
2022-05-05 20:21:07 -0400

    May5 20:20 commit

--- rb101_small_problems/easy3/arithmetic_integer.rb
+++ rb101_small_problems/easy3/arithmetic_integer.rb
@@ -1,2 +1,20 @@
 #Arithmetic integer, easy 3
 
+#input - two positive! integers
+#output -  six answers: addition, subtraction, product, quotient, remainder, power
+
+#rules -Don't worry about validating the input 
+
+#edge cases -
+
+#data structures - integers, operators(?)
+
+#algorithm -  string interpolation  #{}
+
+puts "=> Please input two positive integers. Enter the first number"
+puts "=> Enter the second number"
+user_input.gets.chomp
+
+puts "=> #{input1} + #{input2} = "
+
+#input1, input2 
