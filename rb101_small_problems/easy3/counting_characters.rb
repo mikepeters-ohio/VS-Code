14e5a2712f9513fde00ea02474acda9a58d65c3b
Mike Peters <ckn1901@icloud.com>
2022-05-08 21:18:18 -0400

    May8 2118 commit

--- rb101_small_problems/easy3/counting_characters.rb
+++ rb101_small_problems/easy3/counting_characters.rb
@@ -1,12 +1,31 @@
 #Counting the Number of Characters, Easy 3
 
-#input - 
-#output -  
+#input -  word or words (strings!)
+#output -   A sentence that tells the number of characters in integers
 
-#rules -
+#rules - spaces don't count as characters
 
-#edge cases -
+#edge cases - Hyphened words
 
-#data structures - 
+#data structures -  strings, integers
 
-#algorithm -  
+#algorithm -  string interpolation 
+
+=begin
+def prompt(message)
+  puts "==> #{message}"
+end 
+
+prompt("Please write a word or multiple words:")
+words = gets.chomp.to_i
+
+prompt("There are #{words} characters in ")
+prompt 
+=end 
+
+print 'Please write word or multiple words: '
+input = gets.chomp
+number_of_characters = input.delete(' ').size
+puts "There are #{number_of_characters} characters in \"#{input}\"."
+
+#I can't figure out why this code doesn't run in replit.com
